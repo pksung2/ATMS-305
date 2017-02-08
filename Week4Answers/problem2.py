74b74dd826f52b7a3ae81224ea790fa71e12da8d
Paul Sung <pksung2@illinois.edu>
2017-02-08 10:34:37 -0600

    edited some things

--- Week4Answers/problem2.py
+++ Week4Answers/problem2.py
@@ -4,12 +4,15 @@ import math
 import sys
 import numbers
 
-if len(sys.argv) == 2:
-	T = float(sys.argv[1])
-	sat = 6.1094 * math.exp(17.625*T/(T+243.04))
-	print 'The saturation vapor pressure temperature is',sat
+a = len(sys.argv)
 
-elif len(sys.argv) > 2:
+if a == 2:
+
+		T = float(sys.argv[1])
+                sat = 6.1094 * math.exp(17.625*T/(T+243.04))
+                print 'The saturation vapor pressure temperature is',sat
+
+elif a > 2:
 	print "Too many values."
 
 else:
