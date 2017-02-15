fa1022cb7311041408316882d16f49e228e1f66a
Paul Sung <pksung2@illinois.edu>
2017-02-15 10:15:32 -0600

    Added Week5Answers documents

--- Week5Answers/test.py
+++ Week5Answers/test.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
+#defining the filename
 filename='aravg.ann.land_ocean.90S.90N.v4.0.1.201612.asc'
 wxdata = {'year':[],'temperature':[]}	#define dictionary
 
+#open a file and read its contents
 with open(filename, "r") as f: #read all the lines in the file
 	alist = f.read().splitlines()
 
+#looping over all the lines in the file
 for line in alist: #iterate through lines
 	wxdata['year'].append(int(line.split()[0]))
 	wxdata['temperature'].append(float(line.split()[1]))
 
 print(len(wxdata['year']))
-# print(year)
+
+
+#print(year)
