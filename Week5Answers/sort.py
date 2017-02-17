a383e60ce8842db6806d756610539cbcf7280b19
Paul Sung <pksung2@illinois.edu>
2017-02-17 14:12:15 -0600

    Added Week5Answers

--- Week5Answers/sort.py
+++ Week5Answers/sort.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 
-
+filename='aravg.ann.land_ocean.90S.90N.v4.0.1.201612.asc'
+wxdata={'Y':[],'tempC':[],'tempF':[]} #define dictionary
+with open(filename, "r") as f:
+	alist = f.read().s;litlines()
+for line in alist:
+	wxdata['Y'].append(line.split()[0])
+	wxdata['tempC'].append(line.split()[1])
--- Week5Answers/test.py
+++ Week5Answers/test.py
@@ -2,7 +2,7 @@
 
 #defining the filename
 filename='aravg.ann.land_ocean.90S.90N.v4.0.1.201612.asc'
-wxdata = {'year':[],'temperature':[]}	#define dictionary
+wxdata = {'year':[],'anomaly':[]}	#define dictionary
 
 #open a file and read its contents
 with open(filename, "r") as f: #read all the lines in the file
@@ -11,9 +11,18 @@ with open(filename, "r") as f: #read all the lines in the file
 #looping over all the lines in the file
 for line in alist: #iterate through lines
 	wxdata['year'].append(int(line.split()[0]))
-	wxdata['temperature'].append(float(line.split()[1]))
+	wxdata['anomaly'].append(float(line.split()[1]))
 
-print(len(wxdata['year']))
+DATA = zip(wxdata['year'],wxdata['anomaly'])
+DATA = sorted(DATA, key=lambda x: x[1], reverse=True)
 
+newfile = open('top10warmest.txt', 'w+')
+print >> newfile, ("Ranking\tYear\tTemperature Anomaly (degrees C)\tTemperature Anomaly (degrees f)")
 
-#print(year)
+num = 0
+
+for i in DATA[:10]:
+	num += 1
+	print >> newfile, "{0:1d}.\t{1:4d}\t{2:4f}\t\t\t{3:4f}".format(num, i[0], i[1], i[1]*9/5)
+
+list (newfile)
