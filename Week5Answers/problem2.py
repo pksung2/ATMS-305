7be42fe11060e18f98a5f9972b6fe61fd4ee6ddb
Paul Sung <pksung2@illinois.edu>
2017-02-18 20:25:33 -0600

    Finished HW4

--- Week5Answers/problem2.py
+++ Week5Answers/problem2.py
@@ -12,6 +12,7 @@ with open(filename, "r") as f:
 for line in clist:
         cdict['station'].append(line.split(',')[0])
         cdict['valid'].append(line.split(',')[1])
+        cdict['tmpf'].append(line.split(',')[2])
         cdict['presentwx'].append(line.split(',')[-1])
 
 #initialize SN counter
@@ -20,9 +21,28 @@ SN=0
 #begin incrementing SN counter by searching each string in list
 for i in cdict['presentwx']:
 	if 'SN' in i:
-		print (i)
+#		print (i)
 		SN+=1
+
+#make highest temperature and date available
+temperature = sorted(zip(cdict['tmpf'], cdict['valid']))
+
+#initialize total temperature counter
+total=0
+temptotal=0
+
+#find highest temperature recorded
+for i in temperature:
+        try:
+                float(i[0])
+                temp = i
+		total += 1
+		temptotal+=float(i[0])
+        except ValueError:
+                pass
 
 #print(len(cdict['presentwx']))
 print("Total reports of snow: {0}".format(SN))
-
+print("Fraction of reports with snow: {0}/{1}".format(SN, len(cdict['presentwx'])))
+print("Highest temperature: {0}F at {1}".format(temp[0], temp[1]))
+print("Mean temperature: {0:5.2f}F".format(temptotal/total))
